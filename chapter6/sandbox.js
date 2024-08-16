b28ead39e171ef7c46e7827697399139f9a5f58c
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-08-16 16:49:35 -0400

    clock built

---
+++ chapter6/sandbox.js
@@ -0,0 +1,37 @@
+let now = new Date()
+const clock = document.querySelector('.clock')
+
+console.log(now)
+console.log(now.getFullYear())
+console.log(now.getMonth())
+console.log(now.getDay())
+console.log(now.getDate())
+console.log(now.getHours())
+console.log(now.getMinutes())
+console.log(now.getSeconds())
+console.log(now.getTime())
+console.log(now.toDateString())
+console.log(now.toTimeString())
+console.log(now.toLocaleString())
+
+const tick = () => {
+
+  let date = new Date()
+  let hour = date.getHours()
+  if(hour > 12){
+    hour = hour - 12
+  }
+  let min = date.getMinutes()
+  if(min < 10){
+    min = `0${min}`
+  }
+  let sec = date.getSeconds()
+  if(sec < 10){
+    sec = `0${sec}`
+  }
+  // console.log(hour, min, sec)
+  let time = `${hour}:${min}:${sec}`
+  clock.innerText = time
+}
+
+setInterval(tick, 1000)
