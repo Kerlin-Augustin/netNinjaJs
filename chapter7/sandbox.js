6245e9f3554fd6cfc629b8773215296397bf9ae5
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-08-17 11:44:10 -0400

    async

--- chapter7/sandbox.js
+++ chapter7/sandbox.js
@@ -13,24 +13,24 @@
 //   console.log('callback 2')
 // }, 1000)
 
-const getTodos = (resource) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
+// const getTodos = (resource) => {
+//   return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest();
 
-    request.addEventListener('readystatechange', () => {
-      // console.log(request, request.readyState)
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText)
-        resolve(data)
-      } else if (request.readyState === 4) {
-        reject('error')
-      }
-    })
+//     request.addEventListener('readystatechange', () => {
+//       // console.log(request, request.readyState)
+//       if (request.readyState === 4 && request.status === 200) {
+//         const data = JSON.parse(request.responseText)
+//         resolve(data)
+//       } else if (request.readyState === 4) {
+//         reject('error')
+//       }
+//     })
 
-    request.open('GET', resource);
-    request.send()
-  })
-}
+//     request.open('GET', resource);
+//     request.send()
+//   })
+// }
 
 // getTodos('https://jsonplaceholder.typicode.com/todos/').then(data => {
 //   console.log('promise resolves: ', data)
@@ -62,11 +62,46 @@ const getTodos = (resource) => {
 //     console.log(err)
 //   })
 
-fetch('https://jsonplaceholder.typicode.com/todos/').then((response) => {
-    console.log(response)
-    return response.json()
-}).then(data => {
-  console.log(data)
-}).catch((reject) => {
-  console.log(reject)
-})
+// fetch('https://jsonplaceholder.typicode.com/todos/').then((response) => {
+//     console.log(response)
+//     return response.json()
+// }).then(data => {
+//   console.log(data)
+// }).catch((reject) => {
+//   console.log(reject)
+// })
+
+const getTodos = async () => {
+  try {
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos/')
+
+    if(!response.ok){
+      throw new Error('fetch error')
+    }
+
+    let data = await response.json()
+
+    response = await fetch('https://jsonplaceholder.typicode.com/todos/')
+
+    if(!response.ok){
+      throw new Error('fetch error')
+    }
+    
+    data = await response.json()
+
+    response = await fetch('https://jsonplaceholder.typicode.com/todos/')
+
+    if(!response.ok){
+      throw new Error('fetch error')
+    }
+
+    data = await response.json()
+    return data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+getTodos()
+  .then(data => console.log(`resolve: `, data))
+  .catch(err => console.log(`rejectd: `,err.message))
